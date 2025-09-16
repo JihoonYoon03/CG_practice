86bfaa99cb6c985fe775917ae60fe34764d17069
laptop-msi <wlgns7922@gmail.com>
2025-09-16 21:16:44 +0900

    Add: 우클릭 기능 완성

--- prac2.cpp
+++ prac2.cpp
@@ -2,6 +2,7 @@
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
+#include <cstdlib>
 
 // void glRectf(Glfloat x1, Glfloat y1, Glfloat x2, Glfloat y2);
 // void glColor3f (Glfloat r, Glfloat g, Glfloat b);
@@ -78,18 +79,37 @@ GLvoid Mouse(int button, int state, int x, int y) {
 	switch (button) {
 	case GLUT_LEFT_BUTTON:
 		if (state == GLUT_DOWN) {
-			if (isMouseIn(rects[0], x, y));
-			else if (isMouseIn(rects[1], x, y));
-			else if (isMouseIn(rects[2], x, y));
-			else if (isMouseIn(rects[3], x, y));
-			else randColor(bgColor[0], bgColor[1], bgColor[2]);
-
+			for (int i = 0; i < 4; i++) {
+				if (isMouseIn(rects[i], x, y)) {
+					randColor(rects[i].r, rects[i].g, rects[i].b);
+					break;
+				}
+				else if (i == 3) randColor(bgColor[0], bgColor[1], bgColor[2]);
+			}
+			
 			std::cout << "Left Button Down (" << x << ", " << y << ")\n";
 			glutPostRedisplay();
 		}
 		break;
 	case GLUT_RIGHT_BUTTON:
 		if (state == GLUT_DOWN) {
+			for (int i = 0; i < 4; i++) {
+				if (isMouseIn(rects[i], x, y)) {
+					if (abs(rects[i].x2 - rects[i].x1) > 0.4f && abs(rects[i].y2 - rects[i].y1) > 0.4f) {
+						rects[i].x1 += 0.1f;
+						rects[i].x2 -= 0.1f;
+						rects[i].y1 -= 0.1f;
+						rects[i].y2 += 0.1f;
+					}
+					break;
+				}
+				else if (i == 3) {
+					rects[0] = { -0.9f, 0.9f, -0.1f, 0.1f, rects[0].r, rects[0].g, rects[0].b };
+					rects[1] = { 0.1f, 0.9f, 0.9f, 0.1f, rects[1].r, rects[1].g, rects[1].b };
+					rects[2] = { -0.9f, -0.1f, -0.1f, -0.9f, rects[2].r, rects[2].g, rects[2].b };
+					rects[3] = { 0.1f, -0.1f, 0.9f, -0.9f, rects[3].r, rects[3].g, rects[3].b };
+				}
+			}
 			std::cout << "Right Button Down (" << x << ", " << y << ")\n";
 		}
 		break;
@@ -101,10 +121,7 @@ bool isMouseIn(Rect& target, int x, int y)
 	GLfloat xGL = (x / 400.0f) - 1.0f;
 	GLfloat yGL = 1.0f - (y / 300.0f);
 
-	if (xGL > target.x1 && xGL < target.x2 && yGL < target.y1 && yGL > target.y2) {
-		randColor(target.r, target.g, target.b);
-		return true;
-	}
+	if (xGL > target.x1 && xGL < target.x2 && yGL < target.y1 && yGL > target.y2) return true;
 	else return false;
 }
 
